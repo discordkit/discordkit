d9cff8facd175a8912d862f35ae52800150a1f8d
Drake Costa <drake@saeris.io>
2023-10-02 16:26:42 -0700

    yay tests are finally running! :D

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["packages/client/src/voice/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
     name: `discordkit`,
     globals: true,
     watch: false,
