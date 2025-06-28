b7cbb53945046847fc946131c219e42fddb967dc
Drake Costa <drake@saeris.io>
2025-06-28 12:59:36 -0700

    chore: Update tsconfigs, Add updated apps code

--- apps/webhook-action/src/index.ts
+++ apps/webhook-action/src/index.ts
@@ -1,24 +1,39 @@
+import { nonEmpty, object, pipe, safeParse, string, summarize } from "valibot";
 import { executeWebhookSafe } from "@discordkit/client/webhook/executeWebhook";
 
-void (async (): Promise<void> => {
-  const getInput = (name: string): string =>
-    (
-      process.env[`INPUT_${name.replace(/ /g, `_`).toUpperCase()}`] ?? ``
-    ).trim();
+const isRequired = `must be a string`;
+const isEmpty = `must not be empty`;
 
-  try {
-    console.info(`Running discord webhook action...`);
-    await executeWebhookSafe({
-      webhook: getInput(`webhook`),
-      token: getInput(`token`),
-      body: {
-        content: getInput(`content`)
-      }
-    });
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error(error.message);
+const env = safeParse(
+  object(
+    {
+      INPUT_WEBHOOK: pipe(string(isRequired), nonEmpty(isEmpty)),
+      INPUT_TOKEN: pipe(string(isRequired), nonEmpty(isEmpty)),
+      INPUT_CONTENT: pipe(string(isRequired), nonEmpty(isEmpty))
+    },
+    (issue) => `Required environment variable ${issue.expected} is missing!`
+  ),
+  process.env
+);
+
+if (env.issues) {
+  throw new Error(summarize(env.issues));
+}
+
+const { INPUT_WEBHOOK, INPUT_TOKEN, INPUT_CONTENT } = env.output;
+
+try {
+  console.info(`Running discord webhook action...`);
+  await executeWebhookSafe({
+    webhook: INPUT_WEBHOOK,
+    token: INPUT_TOKEN,
+    body: {
+      content: INPUT_CONTENT
     }
-    process.exit(1);
+  });
+} catch (error) {
+  if (error instanceof Error) {
+    console.error(error.message);
   }
-})();
+  process.exit(1);
+}
